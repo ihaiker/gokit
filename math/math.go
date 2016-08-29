10e7a1b585da789221d47bd1f77945352d9c5acc
haiker <zhouhaichao@2008.sina.com>
2016-08-29 11:36:13 +0800

    add meth

--- math/math.go
+++ math/math.go
@@ -6,13 +6,13 @@ import (
 )
 
 func Scale(num float32, size int) float32 {
-	str, _ := fmt.Printf("%." + size + "f", num)
+	str := fmt.Sprintf(("%." + strconv.Itoa(size) + "f"), num)
 	n, _ := strconv.ParseFloat(str, 32)
 	return float32(n)
 }
 
 func Scale64(num float64, size int) float64 {
-	str, _ := fmt.Printf("%." + size + "f", num)
+	str := fmt.Sprintf(("%." + strconv.Itoa(size) + "f"), num)
 	n, _ := strconv.ParseFloat(str, 64)
 	return n
 }
