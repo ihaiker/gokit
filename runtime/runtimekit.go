9fd09ec9d0dfcd2d8f2ecdff77e3390636a64052
haiker <zhouhaichao@2008.sina.com>
2016-10-13 15:53:46 +0800

    add runtime function

---
+++ runtime/runtimekit.go
@@ -0,0 +1,14 @@
+package runtimekit
+
+import (
+	"runtime"
+	"path"
+)
+
+//返回工作目录
+func GetWorkDir() string {
+	_, filename, _, _ := runtime.Caller(1)
+	baseDir := path.Dir(filename)
+	return baseDir
+}
+
