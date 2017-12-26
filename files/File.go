496d98297f962ad31c52a3fd4bf6b525e8753dd6
haiker <zhouhaichao@2008.sina.com>
2017-12-26 20:25:21 +0800

    日志问题修改

--- files/File.go
+++ files/File.go
@@ -1,109 +1,117 @@
 package fileKit
 
 import (
-	"os"
-	"errors"
-	"path/filepath"
-	"io/ioutil"
-	"github.com/ihaiker/gokit/commons"
+    "os"
+    "errors"
+    "path/filepath"
+    "io/ioutil"
+    "github.com/ihaiker/gokit/commons"
 )
 
 type File struct {
-	path string
+    path string
 }
 
 func New(path string) *File {
-	absPath, _ := filepath.Abs(path)
-	return &File{path:absPath}
+    absPath, _ := filepath.Abs(path)
+    return &File{path: absPath}
 }
 
 func (self *File) IsFile() bool {
-	return !IsDir(self.path)
+    return !IsDir(self.path)
 }
 func (self *File) IsDir() bool {
-	return IsDir(self.path)
+    return IsDir(self.path)
 }
 func (self *File) Exist() bool {
-	return Exist(self.path)
+    return Exist(self.path)
 }
 func (self *File) Name() string {
-	return filepath.Base(self.path)
+    return filepath.Base(self.path)
 }
 
 func (self *File) Mkdir() error {
-	if self.Exist() {
-		return errors.New("the fodler or file is exits")
-	}
-	return os.Mkdir(self.path, os.ModePerm)
+    if self.Exist() {
+        return errors.New("the fodler or file is exits")
+    }
+    return os.Mkdir(self.path, os.ModePerm)
 }
 
 func (self *File) Parent() *File {
-	if filepath.Dir(self.path) == self.path {
-		return nil
-	}
-	return New(filepath.Dir(self.path))
+    if filepath.Dir(self.path) == self.path {
+        return nil
+    }
+    return New(filepath.Dir(self.path))
 }
 
 func (self *File) Equal(file *File) bool {
-	return self.path == file.path
+    return self.path == file.path
 }
 
 func (self *File) List() ([]*File, error) {
-	dir, err := ioutil.ReadDir(self.path)
-	if err != nil {
-		return nil, err
-	}
-	files := make([]*File, len(dir))
-	for idx, finfo := range dir {
-		files[idx] = New(self.path + "/" + finfo.Name())
-	}
-	return files, nil
+    dir, err := ioutil.ReadDir(self.path)
+    if err != nil {
+        return nil, err
+    }
+    files := make([]*File, len(dir))
+    for idx, finfo := range dir {
+        files[idx] = New(self.path + "/" + finfo.Name())
+    }
+    return files, nil
 }
 
 func (self *File) Rename(newName string) error {
-	dir, _ := filepath.Split(self.path)
-	newPath := dir + "/" + newName
-	return os.Rename(self.path, newPath)
+    dir, _ := filepath.Split(self.path)
+    newPath := dir + "/" + newName
+    return os.Rename(self.path, newPath)
 }
 
 func (self *File) GetPath() string {
-	return self.path
+    return self.path
 }
 
 func (self *File) ToString() (string, error) {
-	bs, err := self.ToBytes()
-	return string(bs), err
+    bs, err := self.ToBytes()
+    return string(bs), err
 }
 
 //delete file or folder
 func (self *File) Remove() error {
-	return os.Remove(self.path)
+    return os.Remove(self.path)
 }
+
 //delete file or folder and subfolder
 func (self *File) RemoveAll() error {
-	return os.RemoveAll(self.path)
+    return os.RemoveAll(self.path)
 }
 
 func (self *File) ToBytes() ([]byte, error) {
-	return ioutil.ReadFile(self.path)
+    return ioutil.ReadFile(self.path)
 }
 
 func (self *File) GetWriter(append bool) (*os.File, error) {
-	flag := os.O_RDWR | os.O_CREATE
-	if append && self.Exist() {
-		flag = flag | os.O_APPEND
-	}
-	return os.OpenFile(self.path, flag, 0666)
+    flag := os.O_RDWR | os.O_CREATE
+    if append && self.Exist() {
+        flag = flag | os.O_APPEND
+    }
+    return os.OpenFile(self.path, flag, 0666)
 }
 
 func (self *File) LineIterator() (commonKit.CloseIterator, error) {
-	if self.Exist() && self.IsFile() {
-		f, err := os.OpenFile(self.path, (os.O_RDWR | os.O_APPEND), 0666)
-		if err != nil {
-			return nil, err
-		} else {
-			return newIterator(f), nil
-		}
-	}
-	return nil, errors.New("not found or is not file")
+    if self.Exist() && self.IsFile() {
+        f, err := os.OpenFile(self.path, (os.O_RDWR | os.O_APPEND), 0666)
+        if err != nil {
+            return nil, err
+        } else {
+            return newIterator(f), nil
+        }
+    }
+    return nil, errors.New("not found or is not file")
+}
+
+func (self *File) GetReader() (*os.File, error) {
+    if self.Exist() && self.IsFile() {
+        return os.OpenFile(self.path, (os.O_RDWR | os.O_APPEND), 0666)
+    }
+    return nil, errors.New("not found or is not file")
 }
