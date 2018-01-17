5fd78a5408c4e15f5d763000f5a37b0579b2309c
haiker <zhouhaichao@2008.sina.com>
2018-01-17 23:50:52 +0800

    tcp config

--- tcp/config.go
+++ tcp/config.go
@@ -3,9 +3,14 @@ package tcpKit
 import "time"
 
 type Config struct {
-    PacketSendChanLimit    uint32 // the limit of packet send channel
-    PacketReceiveChanLimit uint32 // the limit of packet receive channel
-    AsynHandler            bool   // asynchronous handler message
-    AcceptTimeout          time.Duration
-    IdleTime, IdleTimeout  int //heartbeat time,and timeout 
+    // the limit of packet send channel
+    PacketSendChanLimit uint32 `json:"packet_send_chan_limit" yaml:"packet_send_chan_limit"`
+    // the limit of packet receive channel
+    PacketReceiveChanLimit uint32 `json:"packet_receive_chan_limit" yaml:"packet_receive_chan_limit"`
+    // asynchronous handler message
+    AsynHandler   bool          `json:"asyn_handler" yaml:"asyn_handler"`
+    AcceptTimeout time.Duration `json:"accept_timeout" yaml:"accept_timeout"`
+    //heartbeat time,and timeout
+    IdleTime    int `json:"idle_time" yaml:"idle_time"`
+    IdleTimeout int `json:"idle_timeout" yaml:"idle_timeout"`
 }
