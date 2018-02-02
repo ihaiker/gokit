a38884ef1f42be2aa9132bfd5215f138cd80cb17
haiker <zhouhaichao@2008.sina.com>
2018-02-03 00:16:41 +0800

    commonKit host ip

---
+++ commons/ip.go
@@ -0,0 +1,54 @@
+package commonKit
+
+import (
+    "net"
+    "github.com/ihaiker/gokit/commons/logs"
+)
+
+func GetHost(IgnoredInterfaces,PreferredNetworks []string) string {
+    ifaces, err := net.Interfaces()
+    if err != nil {
+        logs.Errorf("net interfaces error: %s", err.Error())
+        return "0.0.0.0"
+    }
+    addresies := []string{}
+FACE_LOOP:
+    for _, face := range ifaces {
+        //忽略的网络
+        for _, v := range IgnoredInterfaces {
+            if ( v == face.Name ) {
+                continue FACE_LOOP
+            }
+        }
+        //优先使用的网络
+        preferred := false
+        for _, v := range PreferredNetworks {
+            if ( v == face.Name ) {
+                preferred = true
+                break
+            }
+        }
+
+        if addresss, err := face.Addrs(); err == nil {
+            for _, address := range addresss {
+                if ipNet, ok := address.(*net.IPNet); ok &&
+                    !ipNet.IP.IsLoopback() &&
+                    !ipNet.IP.IsInterfaceLocalMulticast() &&
+                    !ipNet.IP.IsMulticast() &&
+                    !ipNet.IP.IsUnspecified() {
+                    if ipNet.IP.To4() != nil {
+                        if preferred {
+                            return ipNet.IP.String()
+                        } else {
+                            addresies = append(addresies, ipNet.IP.String())
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if len(addresies) != 0 {
+        return addresies[0]
+    }
+    return "0.0.0.0"
+}
