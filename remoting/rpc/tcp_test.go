e84fe1ce1c76bbad6b83373efeb1b32ee0f2dfdd
haiker <wo@renzhen.la>
2019-12-02 19:24:19 +0800

    merge

--- remoting/rpc/tcp_test.go
+++ remoting/rpc/tcp_test.go
@@ -23,7 +23,7 @@ func onMessage(ch remoting.Channel, req *Request) *Response {
 }
 
 func TestRpcServer(t *testing.T) {
-	server, err := NewServer(":6379", onMessage);
+	server, err := NewServer(":6379", onMessage, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -35,11 +35,10 @@ func TestRpcServer(t *testing.T) {
 }
 
 func TestRpcClient(t *testing.T) {
-	client, err := NewClient(":6379", onMessage)
-	if err != nil {
+	client := NewClient(":6379", onMessage, nil)
+	if err := client.Start(); err != nil {
 		t.Fatal(err)
 	}
-	client.Start()
 	time.Sleep(time.Second)
 
 	for i := 0; i < 10; i++ {
