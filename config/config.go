f30d5f9cb703b5a6c5c0b98a6ee99463259ac9d6
haiker <wo@renzhen.la>
2020-03-14 17:31:13 +0800

    fixbug: config

--- config/config.go
+++ config/config.go
@@ -7,16 +7,14 @@ import (
 )
 
 var (
-	ErrConfigNotFound = errors.New("the config not found!")
+	ErrConfigNotFound = errors.New("the config not found")
 )
 
 type register struct {
-	name      string
-	module    string
-	config    *configor.Config
-	path      []string
-	expandEnv bool
-	expand    func(string) string
+	name   string
+	module string
+	config *configor.Config
+	path   []string
 }
 
 func NewConfigRegister(name, module string) *register {
@@ -28,56 +26,46 @@ func NewConfigRegister(name, module string) *register {
 		path: []string{},
 	}
 
-	extends := []string{"toml", "yaml", "json"}
-	for _, ext := range extends {
-		paths := GetStandardConfigurationLocation(name, module, ext)
-		for _, path := range paths {
-			if files.IsExistFile(path) {
-				reg.AddPath(path)
-			}
+	paths := reg.searchConfigLocations()
+	for _, path := range paths {
+		if files.IsExistFile(path) {
+			reg.AddPath(path)
 		}
 	}
 	return reg
 }
 
-func (this *register) SearchConfigLocations() []string {
-	paths := []string{}
-	paths = append(paths, GetStandardConfigurationLocation(this.name, this.module, "json")...)
-	paths = append(paths, GetStandardConfigurationLocation(this.name, this.module, "yaml")...)
+func (this *register) searchConfigLocations() []string {
+	paths := make([]string, 0)
 	paths = append(paths, GetStandardConfigurationLocation(this.name, this.module, "toml")...)
+	paths = append(paths, GetStandardConfigurationLocation(this.name, this.module, "yaml")...)
+	paths = append(paths, GetStandardConfigurationLocation(this.name, this.module, "json")...)
 	return paths
 }
 
 func (this *register) With(config *configor.Config) *register {
 	this.config = config
-	return this;
-}
-
-func (this *register) AddPath(path ...string) *register {
-	this.path = append(this.path, path...)
 	return this
 }
 
-func (this *register) ExpandEnv() {
-	this.expandEnv = true;
+func (this *register) AddPath(path ...string) *register {
+	this.path = append(path, this.path...)
+	return this
 }
 
-func (this *register) Expend(expendFn func(string) string) {
-	this.expand = expendFn;
-}
-
-func (this *register) expendStringValue(cfg interface{}) error {
-	//todo 这里优化
-	return nil
+func (this *register) AddAtPath(at int, path string) *register {
+	if len(this.path) > at {
+		start := this.path[0:at]
+		end := this.path[at:0]
+		this.path = append(start, path)
+		this.path = append(this.path, end...)
+	}
+	return this
 }
 
 func (this *register) Marshal(cfg interface{}) error {
 	if len(this.path) == 0 {
 		return ErrConfigNotFound
 	}
-	if err := configor.New(this.config).Load(cfg, this.path...); err != nil {
-		return err
-	} else {
-		return this.expendStringValue(cfg);
-	}
+	return configor.New(this.config).Load(cfg, this.path...)
 }
--- config/config_test.go
+++ config/config_test.go
@@ -1,13 +1,10 @@
 package config
 
-import "testing"
-
-func TestGetStandardConfigurationLocation(t *testing.T) {
-	path := GetStandardConfigurationLocation("test", "logs", "json")
-	for _, p := range path {
-		t.Log(p)
-	}
-}
+import (
+	"github.com/jinzhu/configor"
+	"os"
+	"testing"
+)
 
 type UserInfo struct {
 	Name    string `json:"name" toml:"name"`
@@ -18,13 +15,22 @@ type UserInfo struct {
 }
 
 func TestRegister(t *testing.T) {
+	_ = os.Setenv("CONFIGOR_DEBUG_MODE", "true")
+	_ = os.Setenv("CONFIGOR_VERBOSE_MODE", "true")
+	_ = os.Setenv("CONFIGOR_SILENT_MODE", "true")
+
 	u := new(UserInfo)
-	cfg := NewConfigRegister("testq", "testq")
+	cfg := NewConfigRegister("test", "test")
+
+	cfg = cfg.With(&configor.Config{
+		ENVPrefix:            "TEST",
+	})
+
 	err := cfg.Marshal(u)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log("name: ", u.Name)
-	t.Log("address.info:", u.Address.Info);
+	t.Log("address.info:", u.Address.Info)
 	t.Log("address.code:", u.Address.Code)
 }
